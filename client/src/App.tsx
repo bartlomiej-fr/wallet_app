3bd649a10bafa9da7d999c16df8b055d40047dd1
Bartek <bartlomiej.fr1@gmail.com>
2023-03-09 23:22:41 +0100

    some style add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
-import './App.css';
+import './App.scss';
 
-function App() {
+function App(): JSX.Element {
     return (
         <div className="App">
             <h1>Wallet application</h1>
-            <form>
-                <label>Login</label>
-                <input type="text" placeholder="User name" />
-                <label>PIN</label>
-                <input type="number" placeholder="PIN" maxLength={4} />
+            <form className="login">
+                <label className="login__lab">Login</label>
+                <input className="login__input" type="text" placeholder="User name" />
+                <label className="login__lab">PIN</label>
+                <input className="login__input" type="number" placeholder="PIN" maxLength={4} />
+                <button>Submit</button>
             </form>
         </div>
     );
